817e1b242e1ff283cbadc88e00fc1b810f2f2b3e
Shaheer Ahmed <shaheerahmed85@gmail.com>
2018-01-19 18:27:09 +0530

    Add files via upload

--- facechatfrontend/WebContent/Friend/FriendController.js
+++ facechatfrontend/WebContent/Friend/FriendController.js
@@ -1,41 +1,75 @@
 myapp.controller("FriendController", function($scope, $http, $location, $rootScope)
 		{
-	      $scope.friend={friendId:0,username:'',friendname:'',status:''};
-	      $scope.allFriendRequest;
-	      
-	      function fetchAllFriendRequest()
-	      {
-	    	  console.log('Fetching all friend requests');
-	    	  $http.get('http://localhost:8080/fcmiddleware/getAllFriendRequest')
-	    	  .then(function(response)
-	    		{
-	    		  $scope.allFriendRequest=response.data;
-	    		  console.log($scope.allFriendRequest);
-	    		});
-	      }
-	      
-	      fetchAllFriendRequest();
-	      
-	      
-	      $scope.reject=function(friendId)
-	      {
-	    	  console.log('Reject for friend Request');
-	    	  $http.get('http://localhost:8080/fcmiddleware/rejectfriendrequest/'+friendId)
-	    	  .success(function(response)
-	    	{
-	    		  console.log('Rejected successfully');
-	    	});
-	      }
-	      
-	      
-	      $scope.approve=function(friendId)
-	      {
-	    	  console.log('approve for friend Request');
-	    	  $http.get('http://localhost:8080/fcmiddleware/approvalfriendrequest/'+friendId)
-	    	  .success(function(response)
-	    	{
-	    		  console.log('approved successfully');
-	    	});
-	      }
-	      
-});//controller
+	$scope.friend={friendId:"",friendname:"",username:"",status:""};
+		var cuser= $rootScope.currentUser.username;
+		$scope.currentuser=cuser;
+		
+			$http.get('http://localhost:8080/fcmiddleware/getAllusers/'+$rootScope.currentUser.username)
+			.then(function(response)
+			{
+			$scope.userdata=response.data;
+		
+			});
+			
+			$http.get('http://localhost:8080/fcmiddleware/getAllFriendRequest/'+$rootScope.currentUser.username)
+			.then(function(response)
+			{
+			$scope.frienddata=response.data;
+		
+			});
+			
+			function getallfriends()
+			{
+				$http.get('http://localhost:8080/fcmiddleware/getAllFriendRequest/'+$rootScope.currentUser.username)
+				.then(function(response)
+				{
+				$scope.frienddata=response.data;
+			
+				});	
+			}
+			
+			
+			$scope.request=function(user)
+			{
+				$scope.clickuser=user;
+			}
+			
+			$scope.addfriend=function()
+			
+			{
+				var cuser= $rootScope.currentUser.username;
+				$scope.friend.friendname=$scope.clickuser.username;
+				$scope.friend.username=cuser;
+				console.log("entered into request friend")
+				$http.post('http://localhost:8080/fcmiddleware/createfriendrequest',$scope.friend)
+			}
+			
+			
+			$scope.acceptrequest=function(friendId)
+			{
+				$http.get('http://localhost:8080/fcmiddleware/approvalfriendrequest/'+friendId).then(getallfriends(),function(response){
+					console.log('friend accepted');
+					
+					});
+			}
+			
+			$scope.deleterequest=function(friendId)
+			{
+				$http.get('http://localhost:8080/fcmiddleware/deletefriend/'+friendId).then(getallfriends(),function(response){
+					console.log('friend deleted');
+					
+					});
+			}
+			
+			
+			$scope.friends=function()
+			{
+				$http.get('http://localhost:8080/fcmiddleware/getapprovefriends/'+$rootScope.currentUser.username)
+				.then(function(response)
+				{
+				$scope.frienddata1=response.data;
+			
+				});	
+			}
+			
+		});
--- facechatfrontend/WebContent/RouteConfig.js
+++ facechatfrontend/WebContent/RouteConfig.js
@@ -1,4 +1,72 @@
 var myapp=angular.module("myApp",['ngRoute','ngCookies']);//'ngCookies'
+myapp.factory('chatService', function($q , $timeout)
+		{
+			console.log("Inside chatService");
+			var base_url="http://localhost:8080/fcmiddleware";
+			
+			var service={},listener=$q.defer(),socket={client:null,stomp:null},messageIds=[];
+
+			service.RECONNECT_TIMEOUT=30000;
+			service.SOCKET_URL=base_url+"/chat";
+			service.CHAT_TOPIC="/topic/message";
+			service.CHAT_BROKER="/app/chat";
+
+			service.send=function(message)
+			{
+				console.log("Inside Chat - send");
+				var id=Math.floor(Math.random()*1000000);
+				socket.stomp.send(service.CHAT_BROKER,{priority:9},JSON.stringify({message:message,id:id}));
+				messageIds.push(id);
+			};
+
+			service.receive=function()
+			{
+				console.log("Inside chat - receive");
+				return listener.promise;
+			};
+
+			var reconnect=function()
+			{
+				console.log("Inside Chat - reconnect");
+				$timeout(function()
+				{
+				initialize();
+				},this.RECONNECT_TIMEOUT);
+			};
+
+			var getMessage=function(data)
+			{
+				console.log("Inside Chat - getMessage");
+				var message=JSON.parse(data),out={};
+				out.message=message.message;
+				out.time=new Date(message.time);
+				return out;
+			};
+
+			var startListener=function()
+			{
+				console.log("Inside Chat - startListener");
+				socket.stomp.subscribe(service.CHAT_TOPIC,function(data)
+				{
+				listener.notify(getMessage(data.body));
+				});
+			};
+
+			var initialize=function()
+			{
+				socket.client=new SockJS(service.SOCKET_URL);
+				socket.stomp=Stomp.over(socket.client);
+				socket.stomp.connect({},startListener);
+				socket.stomp.onclose=reconnect;
+			};
+
+			initialize();
+
+			return service;
+			
+
+		});
+
 
 myapp.config(function($routeProvider)
 		{
@@ -11,10 +79,12 @@ myapp.config(function($routeProvider)
 			               .when("/Logout",{templateUrl:"User/Logout.html"})
 			               .when("/Job",{templateUrl:"Job/Jobinsert.html"})
 				              .when("/Showjob",{templateUrl:"Job/Showjob.html"})
-				              .when("/Friend",{templateUrl:"User/Friend.html"})
-				              .when("/FriendRequest",{templateUrl:"Friend/FriendRequest.html"})
+				              .when("/Friend",{templateUrl:"Friend/Friend.html"})
+				              .when("/UserList",{templateUrl:"Friend/UserList.html"})
 				              .when("/Logout",{templateUrl:"User/Logout.html"})
 				              .when("/ProfilePicture",{templateUrl:"User/ProfilePicture.html"})
+				              
+				              .when("/Chat",{templateUrl:"Chat/Chat.html"})
 				              
 		});
 		myapp.run(function($rootScope,$cookieStore)
